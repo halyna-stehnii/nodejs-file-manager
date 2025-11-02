2417c07bcb1b6a3a491a8bcb515f3a481b9fd5b9
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-11-02 23:14:20 +0100

    feat: implement greetings

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import { createInterface } from "readline";
+import process from "process";
+
+const args = process.argv.slice(2);
+let username = "User";
+
+for (const arg of args) {
+  if (arg.startsWith("--username=")) {
+    username = arg.split("=")[1];
+    break;
+  }
+}
+
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
+});
+
+const exitProgram = () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  rl.close();
+  process.exit(0);
+};
+
+rl.on("SIGINT", () => {
+  process.stdout.write("\n");
+  exitProgram();
+});
+
+rl.on("line", (input) => {
+  const command = input.trim();
+
+  if (command === ".exit") {
+    exitProgram();
+  } else {
+    console.log(`Command received: ${command}`);
+    rl.prompt();
+  }
+});
+
+rl.prompt();
